0d5fb7d39132c88251bf72f61bb7f6ff83af0fe8
tanu <getrjtanyatiwari@gmail.com>
2023-04-16 03:25:33 -0700

    HelloWorld.java is modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        // Creates a reader instance which takes
+        // Creates a hhhhhhhhhhhreader instance which takes
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
